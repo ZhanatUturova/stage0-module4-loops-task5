9af026cbc393879443e40815f2f262d33edcc4f2
Zhanat Uturova <526628@gmail.com>
2023-05-12 02:51:01 +0600

    Complete fifth task of 4.2 module

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,27 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        int sp = 0;
+        int st = height;
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= sp; j++) {
+                    System.out.print(" ");
+                }
+            for (int j = 1; j <= st; j++) {
+                System.out.print("8");
+            }
+            for (int j = 1; j <= sp; j++) {
+                System.out.print(" ");
+            }
+            if (height % 2 != 0 && i <= height / 2 || 
+                height % 2 == 0 && i < height / 2){
+                st -= 2;
+                sp++;
+            } else if ( i > height / 2 && i <= height) {
+                st += 2;
+                sp--;
+            }
+            System.out.println();
+        }
     }
 }
